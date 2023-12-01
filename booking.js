3efd9511548e8382e90ed656c4d9eaf0cd5ba98f
kareemwael9898 <kareemwael9898@gmail.com>
2023-12-01 19:33:54 +0200

    new update

--- booking.js
+++ booking.js
@@ -1,66 +1,61 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const dbConnection = require('../config/database');
-const asyncHandler = require('express-async-handler');
-
-const dotenv = require("dotenv");
-dotenv.config({ path: "../config.env" });
-process.env.TZ = process.env.TIMEZONE;
-
-const app = express();
-const port = 3000;
-
-// Use body-parser middleware to parse POST request data
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Handle POST request to postData
-app.post('/bookings/add', async (req, res) => {
-  // const { id, numPeople, tableNum, startTime, endDate, customerId } = req.body;
-  const bookingId = req.body.bookingId;
+const add = async (req, res) => {
+  const bookingName = req.body.bookingName;
   const numPeople = req.body.numOfPeople;
   const tableNum = req.body.tableNumber;
   const date = req.body.date;
   const startTime = req.body.startTime;
   const endTime = req.body.endTime;
   const customerId = req.body.customerId;
-  console.log('Received data:', { bookingId, numPeople, tableNum, date, startTime, endTime, customerId });
-  var Values = [numPeople, tableNum, date, startTime, endTime, customerId];
-
-  var sql2 = (`SELECT tableNumber,startTime,endTime FROM bookings WHERE date='${date}' And startTime < '${endTime}' And  endTime > '${startTime}' order by startTime;`)
+  console.log('Received data:', { bookingName, numPeople, tableNum, date, startTime, endTime, customerId });
+  
+  var sql2 = (`SELECT tableNumber,startTime,endTime FROM bookings WHERE date='${date}' And tableNumber=${tableNum} And startTime < '${endTime}' And  endTime > '${startTime}' order by startTime;`)
   const [conflictingBookings] = await (await dbConnection).query(sql2);
-
+  
   console.log(conflictingBookings);
   if (conflictingBookings.length === 0) {
-    var sql = 'INSERT  INTO bookings (numberOfPeople,tableNumber,date,startTime,endTime,customerId) VALUES (?,?,?,?,?,?);'
+    var sql = 'INSERT INTO bookings (bookingName,numberOfPeople,tableNumber,date,startTime,endTime,customerId) VALUES (?,?,?,?,?,?,?);'
+    var Values = [bookingName, numPeople, tableNum, date, startTime, endTime, customerId];
     await (await dbConnection).query(sql, Values);
     res.status(200).send('Data received successfully!');
   } else {
-    // res.status(400).send('The Time is Busy!');
+    // res.status(400).send('The Table is Busy!');
     res.status(400).json(conflictingBookings);
   }
-
-});
+  
+};
 
 // Get Bookings By Date
-app.post("/bookings/getByDate", async (req, res) => {
+const getByDate = async (req, res) => {
   const date = req.body.date;
   // console.log(date)
   var sql = (`SELECT tableNumber,startTime,endTime FROM bookings WHERE date='${date}'order by startTime;`);
   const [bookings] = await (await dbConnection).query(sql)
   // console.log(bookings)
   res.status(200).json(bookings)
-})
+};
 
 // Get All Bookings
-app.get("/bookings/getAll", async (req, res) => {
+const getAll = async (req, res) => {
   var sql = (`SELECT * FROM bookings ORDER BY date,startTime;`);
   const [bookings] = await (await dbConnection).query(sql)
   res.status(200).json(bookings)
-})
+};
+
+// Get Busy Tables By Date And Time
+const getBusy = async (req, res) => {
+  const date = req.body.date;
+  const startTime = req.body.startTime;
+  const endTime = req.body.endTime;
+  var sql = (`SELECT tableNumber,startTime,endTime FROM bookings WHERE date='${date}' And startTime < '${endTime}' And  endTime > '${startTime}' order by startTime;`);
+  const [busyTables] = await (await dbConnection).query(sql)
+  res.status(200).json(busyTables)
+};
 
 // Delete Booking By startTime,date,tableNumber and customer Id
-app.post("/bookings/delete", async (req, res) => {
+const deleteBooking = async (req, res) => {
   const date = req.body.date;
   const startTime = req.body.startTime;
   const customerId = req.body.customerId;
@@ -74,13 +69,12 @@ app.post("/bookings/delete", async (req, res) => {
   else {
     res.status(200).json({ DeletedRows: bookings.affectedRows })
   }
-})
+};
 
-app.get("/", (req, res) => {
-  res.sendFile(`/home/kareem/Desktop/index.html`);
-})
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+module.exports = {
+  add,
+  getByDate,
+  getAll,
+  getBusy,
+  deleteBooking
+}
